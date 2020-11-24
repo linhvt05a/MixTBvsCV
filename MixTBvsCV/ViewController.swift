a3c7980b79f09a57da7cd98e345968b1e65e8ce6
linhdevios@gmail.com <linhdevios@gmail.com>
2020-11-24 09:15:14 +0700

    Initial Commit

---
+++ MixTBvsCV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MixTBvsCV
+//
+//  Created by admin on 11/24/20.
+//  Copyright © 2020 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
